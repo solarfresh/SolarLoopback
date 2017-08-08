115e6c358f4bbc79f2a4187ee36dac47473762ed
SolarFresh <shangyuhuang@gmail.com>
2017-08-08 23:22:06 +0800

    add console log to check remote after

--- common/models/ClassifiedImage.js
+++ common/models/ClassifiedImage.js
@@ -1,4 +1,11 @@
 
 module.exports = function(ClassifiedImage) {
+  ClassifiedImage.afterRemote('upload', function(ctx, modelInstance, next) {
+    console.log("ctx:")
+    console.log(ctx)
+    console.log("instance")
+    console.log(modelInstance)
 
+    next();
+  });
 };
