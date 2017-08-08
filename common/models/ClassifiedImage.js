124f29e2cc6fc8490c10e4d1e4953dbdd8205c82
SolarFresh <shangyuhuang@gmail.com>
2017-08-08 23:29:36 +0800

    to parse objects more detail

--- common/models/ClassifiedImage.js
+++ common/models/ClassifiedImage.js
@@ -2,9 +2,9 @@
 module.exports = function(ClassifiedImage) {
   ClassifiedImage.afterRemote('upload', function(ctx, modelInstance, next) {
     console.log("ctx:")
-    console.log(ctx)
+    console.log(ctx.instance)
     console.log("instance")
-    console.log(modelInstance)
+    console.log(modelInstance.result.files.file)
 
     next();
   });
